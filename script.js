6ef8466c1b6488dc301b6a37ea322afd09cdcb28
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-08 11:59:40 +0530

    1 file modified

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ console.log('Hello!');
 // for (let i = 2; i <= 10; i += 2) console.log(i);
 // let i = 0;
 // while (i < 3) {
-  // alert('number ${1}!');
-// }
+// console.log(i);}
 // i++;
+// let number = prompt('enter a number');
+// if (number > 100) {
+// alert('cancel the input');
+// } else {
+// alert('input again');
+// }
+
+// let num = prompt('enter a number greater than 100');
+// for (let num = 0; num <= 100; ) {
+  // console.log(num);
+// }
+let browser=('enter your browser name');
+
+
+
+
