5d8ba6033a518cf155a076d83f89449aad79b9b5
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-08 11:35:22 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log('Hello!');
-for(let i=0; i<5;i++)
-console.log(i);
+// for (let i = 0; i < 5; i++) console.log(i);
+// for (let i = 0; i < 5; ++i) console.log(i);
+// for (let i = 2; i <= 10; i += 2) console.log(i);
+// let i = 0;
+// while (i < 3) {
+  // alert('number ${1}!');
+// }
+// i++;
