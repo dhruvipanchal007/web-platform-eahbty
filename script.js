240546b8858a98f4807c6f14fc722d813a83a963
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-08 12:08:45 +0530

    1 file modified

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ console.log('Hello!');
 // console.log(num);
 // }
 let browser = prompt('enter your browser name');
-if (edge) {
-  console.log('you have got the edge!');
-} else if (crome) {
+if (browser === 'edge') {
+  alert('you have got the edge!');
+} else if (browser === 'crome') {
   console.log('okay,we support this browser');
-} else if (firefox) {
+} else if (browser === 'firefox') {
   console.log('okay,we support this browser');
-} else if (safari) {
+} else if (browser === 'safari') {
   console.log('okay,we support this browser');
-} else if (opera) {
+} else if (browser === 'opera') {
   console.log('okay,we support this browser');
 } else {
   console.log('we hope that this page look okay');
