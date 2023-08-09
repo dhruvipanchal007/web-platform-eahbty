ee8d0dcd997ab7730f21013fdce66c5ff6e32f3c
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-09 11:50:24 +0530

    1 file modified

--- script.js
+++ script.js
@@ -17,17 +17,134 @@ console.log('Hello!');
 // for (let num = 0; num <= 100; ) {
 // console.log(num);
 // }
-let browser = prompt('enter your browser name');
-if (browser === 'edge') {
-  alert('you have got the edge!');
-} else if (browser === 'crome') {
-  console.log('okay,we support this browser');
-} else if (browser === 'firefox') {
-  console.log('okay,we support this browser');
-} else if (browser === 'safari') {
-  console.log('okay,we support this browser');
-} else if (browser === 'opera') {
-  console.log('okay,we support this browser');
-} else {
-  console.log('we hope that this page look okay');
-}
+// let browser = prompt('enter your browser name');
+// if (browser === 'edge') {
+// alert('you have got the edge!');
+// } else if (browser === 'crome') {
+// console.log('okay,we support this browser');
+// } else if (browser === 'firefox') {
+// console.log('okay,we support this browser');
+// } else if (browser === 'safari') {
+// console.log('okay,we support this browser');
+// } else if (browser === 'opera') {
+// console.log('okay,we support this browser');
+// } else {
+// console.log('we hope that this page look okay');
+// }
+
+// let age = prompt('how many years old?');
+// if (age > 18 || age < 25) {
+// console.log(true);
+// } else {
+// console.log(false);
+// }
+// function checkAge(age) {
+// return age > 18 ? true : confirm('did parents allow you?');
+// }
+// checkAge();
+
+// function min(a, b) {
+// if (5 < 7) {
+// return a;
+// } else {
+// return b;
+// }
+// }
+// min();
+// console.log('hello');
+
+// function printpyramid(rows){
+// for(let i=1;i<=10;i++)
+// }
+
+// grade total=70+85+80+90+75
+// let a = 70;
+// let b = 85;
+// let c = 80;
+// let d = 90;
+// let e = 56;
+
+// console.log(a + b + c + d + e);
+// function calculateAverage(a, b, c, d, e) {
+// const avg = (a + b + c + d + e) / 5;
+// return avg;
+// }
+// let gradetotal = 400;
+// let gradelength = 5;
+// console.log(gradetotal / gradelength);
+// const ans = calculateAverage(a, b, c, d, e);
+// console.log(ans);
+
+// function calculateSimpleInterest(p, r, n) {
+// let i = (p * r * n) / 100;
+// return i;
+// }
+// const interest = calculateSimpleInterest(500000, 7.25, 2);
+// console.log(interest);
+
+// {ex=1}
+// function calculateAverage(a, b, c, d, e) {
+// let i = (70 + 85 + 80 + 90 + 75) / 5;
+// return i;
+// }
+// const average = calculateAverage(70 + 85 + 80 + 90 + 75);
+// console.log(average);
+
+// {ex=2}
+// let a = 5;
+// let b = 20;
+// let c = 30;
+// let d = 25;
+// function findMaxnumber(numbers) {
+// var maxnumber = numbers[0];
+// for (let i = 1; i < numbers.length; i++) {
+// if (numbers[i] > maxnumber) {
+// maxnumber = numbers[i];
+// }
+// }
+// return maxnumber;
+// }
+// var numberlist = [10, 20, 30, 40, 5];
+// var largestnumber = findMaxnumber(numberlist);
+// console.log(largestnumber);
+
+// {ex=3}
+// function calculateCompandinterest(p, r, n) {
+//   let i = (p * r * n) / 100;
+//   return i;
+// }
+// const compandinterest = calculateCompandinterest(10000 * 5 * 3);
+// console.log(compandinterest);
+
+// function cci(p, r, t) {
+//   let amount = p * (1 + r / 100) ** t;
+//   let ci = amount - p;
+//   console.log('interest is : ' + ci);
+// }
+
+// let p = 1200;
+// let r = 5.4;
+// let t = 2;
+// cci(p, r, t);
+
+// p = 1000;
+// r = 6;
+// t = 3;
+// cci(p, r, t);
+
+// p = 2000;
+// r = 7;
+// t = 4;
+// cci(p, r, t);
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// let no1 = 10;
+// let no2 = 20;
+// sum(no1, no2);
+
+// no1 = 20;
+// no2 = 30;
+// sum(no1, no2);
