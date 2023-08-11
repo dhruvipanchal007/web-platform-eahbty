3b12305b4699bf8a2ce9faf64e50ad339e613387
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-11 18:08:38 +0530

    1 file modified

--- questions/001.js
+++ questions/001.js
@@ -1,10 +1,10 @@
-function calcSimpleInterest(p,r,n){
-  let i=(p*r*n)/100;
+function calcSimpleInterest(p, r, n) {
+  let i = (p * r * n) / 100;
   return i;
 }
-let p=prompt('enter principle amount');
-let r=prompt('enter rate of interest');
-let n=prompt('enter number of years');
+let p = prompt('enter principle amount');
+let r = prompt('enter rate of interest');
+let n = prompt('enter number of years');
 
-const SI=calcSimpleInterest(p,r,n);
+const SI = calcSimpleInterest(p, r, n);
 alert(SI);
