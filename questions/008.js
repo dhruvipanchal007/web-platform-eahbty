a9ed8fe6a18d63b68c81dfd5fc158636f5908baf
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-12 18:36:30 +0530

    1 file modified

--- questions/008.js
+++ questions/008.js
@@ -1,11 +1,11 @@
 function searchbook(booksname) {
   let searchtitle = 'science';
   for (let i = 0; i < booksname.length; i++) {
-    if (searchtitle.toLowerCase === booksname[i]) {
+    if (searchtitle === booksname[i]) {
       return searchtitle;
     }
   }
 }
-let booksname = ['science', 'gujarati', 'maths', 'g.k', 'english'];
+let booksname = prompt('enter book name');
 const findbook = searchbook(booksname);
 alert(findbook);
