dc9a84b802f6b5a98956176ac62026ef5c0258b2
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-12 18:12:12 +0530

    2 files modified

--- questions/006.js
+++ questions/006.js
@@ -1,13 +1,13 @@
-function findmaxnumber(numbers) {
-  let maxnumber = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (maxnumber < numbers[i]) {
-      maxnumber = numbers[i];
-    }
-  }
-  return maxnumber;
-}
+// function findmaxnumber(numbers) {
+//   let maxnumber = numbers[0];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (maxnumber < numbers[i]) {
+//       maxnumber = numbers[i];
+//     }
+//   }
+//   return maxnumber;
+// }
 
-let numbers = [5, 10, 50, 100];
-const largenumber = findmaxnumber(numbers);
-console.log(largenumber);
+// let numbers = [5, 10, 50, 100];
+// const largenumber = findmaxnumber(numbers);
+// console.log(largenumber);
