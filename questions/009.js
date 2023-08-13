75962d7e8047cf6eab622c3e58ef4ca3e44ebc17
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-13 10:20:52 +0530

    2 files modified

--- questions/009.js
+++ questions/009.js
@@ -1,8 +1,8 @@
-function convertmetrictoimperial(metricmeasurement) {
-  let conversionfactor = 3.28084;
-  let imperialunits = metricmeasurement * conversionfactor;
-  return imperialunits;
-}
-let metricmeasurement = prompt('enter metricmeasurement in meter');
-const IU = convertmetrictoimperial(metricmeasurement);
-alert(IU);
+// function convertmetrictoimperial(metricmeasurement) {
+//   let conversionfactor = 3.28084;
+//   let imperialunits = metricmeasurement * conversionfactor;
+//   return imperialunits;
+// }
+// let metricmeasurement = prompt('enter metricmeasurement in meter');
+// const IU = convertmetrictoimperial(metricmeasurement);
+// alert(IU);
