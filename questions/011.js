29398a5a220f3938dbdbb666fb7df96e390e0cd0
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-14 09:42:50 +0530

    2 files modified

--- questions/011.js
+++ questions/011.js
@@ -1,10 +1,10 @@
-function calcburningcalories(r, y, j, c) {
-  let exercise = +r + +y + +j + +c;
-  return exercise;
-}
-let r = prompt('enter your running calories');
-let y = prompt('enter your yoga calories');
-let j = prompt('enter your jumping calories');
-let c = prompt('enter your cycling calories');
-const caloriesburning = calcburningcalories(r, y, j, c);
-alert(caloriesburning);
+// function calcburningcalories(r, y, j, c) {
+//   let exercise = +r + +y + +j + +c;
+//   return exercise;
+// }
+// let r = prompt('enter your running calories');
+// let y = prompt('enter your yoga calories');
+// let j = prompt('enter your jumping calories');
+// let c = prompt('enter your cycling calories');
+// const caloriesburning = calcburningcalories(r, y, j, c);
+// alert(caloriesburning);
