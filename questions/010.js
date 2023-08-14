a56f30e96d6ae87dcd15447523264c3dace109b0
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-14 09:33:07 +0530

    1 file modified

--- questions/010.js
+++ questions/010.js
@@ -1,9 +1,9 @@
 function result(playerscore) {
   let winningscore = 85;
   if (playerscore >= winningscore) {
-    alert('you won the game');
+    return 'you won the game';
   } else {
-    alert('sorry!you can not catch game');
+    return 'sorry!you can not catch game';
   }
 }
 let playerscore = prompt('enter your score');
