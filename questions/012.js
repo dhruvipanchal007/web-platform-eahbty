afdc2b4d38db33685da3477302db67a616973401
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-14 09:53:14 +0530

    2 files modified

--- questions/012.js
+++ questions/012.js
@@ -1,7 +1,7 @@
-function convertfromfaherenheittocelsius(f) {
-  let c = ((f - 32) * 5) / 9;
-  return c;
-}
-let f = prompt('enter Fahrenheit tempreture');
-const celsius = convertfromfaherenheittocelsius(f);
-alert(celsius, 'celsius');
+// function convertfromfaherenheittocelsius(f) {
+//   let c = ((f - 32) * 5) / 9;
+//   return c;
+// }
+// let f = prompt('enter Fahrenheit tempreture');
+// const celsius = convertfromfaherenheittocelsius(f);
+// alert(celsius, 'celsius');
