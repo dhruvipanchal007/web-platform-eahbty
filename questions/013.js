006b632380db7895f8176f3ed02fc5835b53eeaf
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-14 10:29:56 +0530

    2 files modified

--- questions/013.js
+++ questions/013.js
@@ -1,16 +1,16 @@
-function assignGrade(score) {
-  if (score >= 90) {
-    return 'A';
-  } else if (score >= 85) {
-    return 'B';
-  } else if (score >= 80) {
-    return 'c';
-  } else if (score >= 75) {
-    return 'D';
-  } else {
-    return 'E';
-  }
-}
-let score = [prompt('enter your score')];
-const Grade = assignGrade(score);
-alert(Grade);
+// function assignGrade(score) {
+//   if (score >= 90) {
+//     return 'A';
+//   } else if (score >= 85) {
+//     return 'B';
+//   } else if (score >= 80) {
+//     return 'c';
+//   } else if (score >= 75) {
+//     return 'D';
+//   } else {
+//     return 'E';
+//   }
+// }
+// let score = [prompt('enter your score')];
+// const Grade = assignGrade(score);
+// alert(Grade);
