b9ed75d551c58fd19467980a8c6a646dcf4b3296
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-14 11:54:12 +0530

    3 files modified, 1 added

--- questions/014.js
+++ questions/014.js
@@ -5,12 +5,23 @@ function findInappropriatecomment(usercomment) {
     if (usercomment.includes(keyword)) {
       return true;
     }
-    return false;
   }
+  return false;
 }
+// debugger;
 let usercomment = prompt('enter your comment');
 if (findInappropriatecomment(usercomment)) {
   alert('your comments is inappropriate language');
 } else {
   alert('thank you for sharing');
 }
+
+function test() {
+  for (let i = 0; i < 3; i++) {
+    console.log('hi'); // hi
+    return 10;
+  }
+  return 20;
+}
+
+test(); //
--- questions/015.js
+++ questions/015.js
@@ -1,10 +1,10 @@
-function calcSpendingTime(z, p, g, m) {
-  let spendingtime = +z + +p + +g + +m;
-  return spendingtime;
-}
-let z = prompt('how much time spend in zoo');
-let p = prompt('how much time spend in park');
-let g = prompt('how much time spend in garden');
-let m = prompt('how much time spend in museam ');
-const AT = calcSpendingTime(z, p, g, m);
-alert(AT);
+// function calcSpendingTime(z, p, g, m) {
+//   let spendingtime = +z + +p + +g + +m;
+//   return spendingtime;
+// }
+// let z = prompt('how much time spend in zoo');
+// let p = prompt('how much time spend in park');
+// let g = prompt('how much time spend in garden');
+// let m = prompt('how much time spend in museam ');
+// const AT = calcSpendingTime(z, p, g, m);
+// alert(AT);
